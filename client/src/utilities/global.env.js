ce7377fa8c5528ff8f475c1640ec092fc6740409
jksingh1504 <jyotikamal.official@gmail.com>
2024-04-04 15:23:03 +0530

    backend url added in frontend

--- client/src/utilities/global.env.js
+++ client/src/utilities/global.env.js
@@ -1,2 +1,2 @@
-exports.baseURL = "http://localhost:3000/"
-exports.apiURL = "http://localhost:8000/api"
+exports.baseURL = "http://localhost:3000/";
+exports.apiURL = "https://healthcarezone-server.vercel.app/api";
